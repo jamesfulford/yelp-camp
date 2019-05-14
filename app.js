195049286e7932f1e06109f41b92d9b7519cc51f
James <james.fulford@outlook.com>
2019-05-14 03:03:17 +0000

    feat(app): Connection string env variable

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var Comment = require("./models/comment");
 // Configure
 //
 var app = express();
-mongoose.connect("mongodb://localhost/yelpcamp", { useMongoClient: true });
+mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost/yelpcamp', { useMongoClient: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
